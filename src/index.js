2f424d59bf891a3b7f794c82cea6906fd49a5d1e
Ankur Mukherjee <mukherjeeankur0073@gmail.com>
2025-05-31 13:45:39 +0530

    Added almost add base backend services and made a valina user table

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import express from 'express'
 import { app } from './app.js';
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/ApiErrors.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from 'jsonwebtoken';
+import {User} from "../models/user.models.js"
+
+// middleware have next to it as a input
+// as no response is there so _ in place of res
+export const verfiyJWT=asyncHandler(async(req,_,next)=>{
+    try {
+       const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        if(!token){
+            throw new ApiError(401,"Unauthorised request")
+        }
+        console.log(token);
+        const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+    
+       const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+       if(!user){
+        // frontend todo
+        throw new ApiError(401,"Inavalid Access Token");
+       }
+    
+       req.user=user;
+       next();
+    } catch (error) {
+        throw new ApiError(401,error?.message||"Invalid access token");
+    }
+
+})
