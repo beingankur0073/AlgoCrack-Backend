dab5b07127c3a79436420d4c81e4727cb6cbd133
Ankur Mukherjee <mukherjeeankur0073@gmail.com>
2025-08-03 20:13:35 +0530

    Added partitioned attribute in options

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -82,14 +82,7 @@ const registerUser=asyncHandler(async(req,res)=>{
 
 
 const loginUser=asyncHandler(async(req,res)=>{
-   // req body -> data
-   // username or email
-   // find the user
-   // password check
-   // access and refresh token
-   // send cookie
 
- 
    const {email,username,password}=req.body;
 
    if(!username && !email){
@@ -119,6 +112,7 @@ const loginUser=asyncHandler(async(req,res)=>{
      httpOnly:true,
      secure:true,
      sameSite: 'None',
+     partitioned: true,
   }
 
   return res.status(200)
